15f899f10f2f750566f61380d1fec3f2dc4e9b81
yusuf <yusufkaleemkhan@gmail.com>
2022-09-11 14:11:16 +0530

    intial test cases

---
+++ src/component/form/Form.test.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { render, screen } from "@testing-library/react"
+import '@testing-library/jest-dom'
+
+describe("Form priority cases", () => {
+
+    test("user should be able to submit form and recieve status", () => {
+        //throw new Error("err")
+    })
+
+    test("user should be able to fill the form feilds", () => {
+        //throw new Error("err")
+    })
+
+})
+
+describe("Form validation", () => {
+
+    test("user should not be able to submit if fiels are empty", () => {
+        //throw new Error("err")
+    })
+
+})
+
+describe("Form basic test", () => {
+
+    test("form should render properly", () => {
+        //throw new Error("err")
+    })
+
+    test("form should display header/title as per passed props", () => {
+        //throw new Error("err")
+    })
+
+    test("the form submit button should be disabled at initial load", () => {
+        //throw new Error("err")
+    })
+
+})
